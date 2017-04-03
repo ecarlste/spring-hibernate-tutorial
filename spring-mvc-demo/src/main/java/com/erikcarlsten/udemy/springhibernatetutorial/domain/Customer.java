717afd223c8a45c6f4ddec0b2aacc8b3311807d0
Erik S Carlsten <ecarlste@gmail.com>
2017-04-03 16:54:51 -0500

    Added Customer field to keep track of number of free passes they have
    Also added corresponding thymeleaf code to the entry form and confirmation templates

--- spring-mvc-demo/src/main/java/com/erikcarlsten/udemy/springhibernatetutorial/domain/Customer.java
+++ spring-mvc-demo/src/main/java/com/erikcarlsten/udemy/springhibernatetutorial/domain/Customer.java
@@ -2,6 +2,8 @@ package com.erikcarlsten.udemy.springhibernatetutorial.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -13,5 +15,10 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min=1, message = "is required")
     private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "Must be an integer value greater than or equal to 0")
+    @Max(value = 10, message = "Must be an integer value less than or equal to 10")
+    private Integer freePasses;
 
 }
