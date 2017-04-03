3ffe5b616485298028cdf3d046a9400b9b067244
Erik S Carlsten <ecarlste@gmail.com>
2017-04-03 15:46:04 -0500

    Added Customer data entity with data validation parameters defined

---
+++ spring-mvc-demo/src/main/java/com/erikcarlsten/udemy/springhibernatetutorial/domain/Customer.java
@@ -0,0 +1,17 @@
+package com.erikcarlsten.udemy.springhibernatetutorial.domain;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class Customer {
+
+    private String firstName;
+
+    @NotNull(message="is required")
+    @Size(min=1)
+    private String lastName;
+
+}
