b59ea58a2417571e9566687b3a1f533095d66641
Erik S Carlsten <ecarlste@gmail.com>
2017-04-04 20:58:55 -0500

    Added JPA/MySQL support to the project and the Student entity

--- spring-mvc-demo/src/main/java/com/erikcarlsten/udemy/springhibernatetutorial/domain/Student.java
+++ spring-mvc-demo/src/main/java/com/erikcarlsten/udemy/springhibernatetutorial/domain/Student.java
@@ -2,10 +2,19 @@ package com.erikcarlsten.udemy.springhibernatetutorial.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
 
 @Data
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
     private String firstName;
     private String lastName;
