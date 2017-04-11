b03a5bb80d2a80f044e1e09fd42f108b9eceed46
Erik S Carlsten <ecarlste@gmail.com>
2017-04-11 17:00:40 -0500

    Added a DAO method to return all customers sorted by last name

--- spring-mvc-demo/src/main/java/com/erikcarlsten/udemy/springhibernatetutorial/service/RemoteCustomerService.java
+++ spring-mvc-demo/src/main/java/com/erikcarlsten/udemy/springhibernatetutorial/service/RemoteCustomerService.java
@@ -2,24 +2,36 @@ package com.erikcarlsten.udemy.springhibernatetutorial.service;
 
 import com.erikcarlsten.udemy.springhibernatetutorial.domain.Customer;
 import com.erikcarlsten.udemy.springhibernatetutorial.domain.CustomerRepository;
-import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
-@AllArgsConstructor
 @Service
 public class RemoteCustomerService implements CustomerService {
 
-    private final CustomerRepository customerRepository;
+    private static final Logger logger = LoggerFactory.getLogger(RemoteCustomerService.class);
+
+    private CustomerRepository customerRepository;
+
+    private RemoteCustomerService() {
+    }
+
+    @Autowired
+    public RemoteCustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> getAllCustomers() {
-        return StreamSupport.stream(customerRepository.findAll().spliterator(), false)
-                .collect(Collectors.toList());
+        List<Customer> sorted = customerRepository.findAllByOrderByLastName();
+
+        logger.info("sorted: {}", sorted);
+
+        return sorted;
     }
 
     @Override
