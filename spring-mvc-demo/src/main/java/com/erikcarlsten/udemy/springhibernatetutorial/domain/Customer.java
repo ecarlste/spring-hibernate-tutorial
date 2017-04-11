a9a36406f3e62f6899a6dae7971e0caae664f622
Erik S Carlsten <ecarlste@gmail.com>
2017-04-10 20:41:46 -0500

    Added customer crud repository for accessing customer database table

--- spring-mvc-demo/src/main/java/com/erikcarlsten/udemy/springhibernatetutorial/domain/Customer.java
+++ spring-mvc-demo/src/main/java/com/erikcarlsten/udemy/springhibernatetutorial/domain/Customer.java
@@ -1,28 +1,31 @@
 package com.erikcarlsten.udemy.springhibernatetutorial.domain;
 
-import com.erikcarlsten.udemy.springhibernatetutorial.web.validation.CourseCode;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
-import javax.validation.constraints.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NonNull
     private String firstName;
 
-    @NotNull(message = "is required")
-    @Size(min=1, message = "is required")
+    @NonNull
     private String lastName;
 
-    @NotNull(message = "is required")
-    @Min(value = 0, message = "Must be an integer value greater than or equal to 0")
-    @Max(value = 10, message = "Must be an integer value less than or equal to 10")
-    private Integer freePasses;
-
-    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 letters/digits")
-    private String postalCode;
-
-    @CourseCode
-    private String courseCode;
+    private String email;
 
 }
