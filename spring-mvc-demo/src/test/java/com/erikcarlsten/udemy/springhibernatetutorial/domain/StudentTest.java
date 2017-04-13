8e41461663788195537dcc5f72ee569895f698c2
Erik S Carlsten <ecarlste@gmail.com>
2017-04-13 18:45:56 -0500

    Added DataJpaTest to check that the student is persisted when saved

--- spring-mvc-demo/src/test/java/com/erikcarlsten/udemy/springhibernatetutorial/domain/StudentTest.java
+++ spring-mvc-demo/src/test/java/com/erikcarlsten/udemy/springhibernatetutorial/domain/StudentTest.java
@@ -4,9 +4,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
+@DataJpaTest
 public class StudentTest {
 
     private static final String FIRST_NAME = "firstname";
@@ -16,6 +22,9 @@ public class StudentTest {
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
+
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Test
     public void createShouldThrowExceptionWhenFirstNameIsNull() throws Exception {
@@ -43,6 +52,14 @@ public class StudentTest {
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage(LAST_NAME_EMPTY_MESSAGE);
         new Student(FIRST_NAME, "");
+    }
+
+    @Test
+    public void saveShouldPersistData() throws Exception {
+        Student student = entityManager.persistAndFlush(new Student(FIRST_NAME, LAST_NAME));
+        assertThat(student.getId()).isNotNull();
+        assertThat(student.getFirstName()).isEqualTo(FIRST_NAME);
+        assertThat(student.getLastName()).isEqualTo(LAST_NAME);
     }
 
 }
