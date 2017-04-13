8607edb1f2d873e5fc5fe9623b49fcdee840f693
Erik S Carlsten <ecarlste@gmail.com>
2017-04-13 18:06:02 -0500

    Added DataJpaTest that checks data persistence for customer saves

--- spring-mvc-demo/src/test/java/com/erikcarlsten/udemy/springhibernatetutorial/domain/CustomerTest.java
+++ spring-mvc-demo/src/test/java/com/erikcarlsten/udemy/springhibernatetutorial/domain/CustomerTest.java
@@ -4,8 +4,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -19,6 +23,9 @@ public class CustomerTest {
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
+
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Test
     public void createShouldThrowExceptionWhenFirstNameIsNull() throws Exception {
@@ -46,6 +53,15 @@ public class CustomerTest {
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage(LAST_NAME_EMPTY_MESSAGE);
         new Customer(FIRST_NAME, "", EMAIL);
+    }
+
+    @Test
+    public void saveShouldPersistData() throws Exception {
+        Customer customer = entityManager.persistFlushFind(new Customer(FIRST_NAME, LAST_NAME, EMAIL));
+        assertThat(customer.getId()).isNotNull();
+        assertThat(customer.getFirstName()).isEqualTo(FIRST_NAME);
+        assertThat(customer.getLastName()).isEqualTo(LAST_NAME);
+        assertThat(customer.getEmail()).isEqualTo(EMAIL);
     }
 
 }
