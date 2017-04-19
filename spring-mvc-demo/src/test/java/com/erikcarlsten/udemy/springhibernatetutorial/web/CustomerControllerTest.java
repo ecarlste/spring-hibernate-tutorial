15b2eeeb63bf2c69d11868626fee7307f88043d5
Erik S Carlsten <ecarlste@gmail.com>
2017-04-18 20:04:48 -0500

    Added Spring MVC slice test for listCustomers get mapping

---
+++ spring-mvc-demo/src/test/java/com/erikcarlsten/udemy/springhibernatetutorial/web/CustomerControllerTest.java
@@ -0,0 +1,51 @@
+package com.erikcarlsten.udemy.springhibernatetutorial.web;
+
+import com.erikcarlsten.udemy.springhibernatetutorial.domain.Customer;
+import com.erikcarlsten.udemy.springhibernatetutorial.service.CustomerService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private CustomerService customerService;
+
+    @Test
+    public void listCustomersShouldListCustomersTemplate() throws Exception {
+        List<Customer> allCustomers = new ArrayList<>();
+        allCustomers.add(new Customer("Erik", "Carlsten", "eshizzle@foo.bar"));
+        allCustomers.add(new Customer("Adnyl", "Rebuts", "llfosho@bar.baz"));
+
+        given(customerService.getAllCustomers()).willReturn(allCustomers);
+
+        mvc.perform(get("/customer/list").accept(MediaType.ALL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("views/list-customers"))
+                .andExpect(MockMvcResultMatchers.view().name("views/list-customers"))
+                .andExpect(model().attribute("customers", allCustomers));
+    }
+
+}
